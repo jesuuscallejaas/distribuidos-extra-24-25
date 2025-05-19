df61e5ff4f53f96e2cbc5d5b37f48d3b59af3864
jesuuscallejaas <jesus.callejas@alu.uclm.es>
2025-05-19 10:22:58 +0200

    fix: rename calculator methods to match interface

--- calculator/calculator.py
+++ calculator/calculator.py
@@ -7,19 +7,19 @@ import RemoteCalculator
 class Calculator(RemoteCalculator.Calculator):
     """Calculator implementation."""
 
-    def add(self, a: float, b: float, current: Ice.Current = None) -> float:
+    def sum(self, a: float, b: float, current: Ice.Current = None) -> float:
         """Add two floats."""
         return a + b
 
-    def subtract(self, a: float, b: float, current: Ice.Current = None) -> float:
+    def sub(self, a: float, b: float, current: Ice.Current = None) -> float:
         """Subtract two floats."""
         return a - b
 
-    def multiply(self, a: float, b: float, current: Ice.Current = None) -> float:
+    def mult(self, a: float, b: float, current: Ice.Current = None) -> float:
         """Multiply two floats."""
         return a * b
 
-    def divide(self, a: float, b: float, current: Ice.Current = None) -> float:
+    def div(self, a: float, b: float, current: Ice.Current = None) -> float:
         """Divide two floats."""
         if b == 0.0:
             raise RemoteCalculator.ZeroDivisionError()
