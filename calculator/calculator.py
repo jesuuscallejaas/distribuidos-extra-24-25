bd776601472bd390ff818ef3f9510023765a01b0
jesuuscallejaas <jesus.callejas@alu.uclm.es>
2025-05-19 10:02:59 +0200

    feat: ice implementation

--- calculator/calculator.py
+++ calculator/calculator.py
@@ -1,6 +1,26 @@
 """Calculator implementation."""
 
-## Mmmmm... here should be some missing imports
+import Ice
+import RemoteCalculator
 
-class Calculator:
-    """BAD skeleton for the implementation (missing parent class)."""
+
+class Calculator(RemoteCalculator.Calculator):
+    """Calculator implementation."""
+
+    def add(self, a: float, b: float, current: Ice.Current = None) -> float:
+        """Add two floats."""
+        return a + b
+
+    def subtract(self, a: float, b: float, current: Ice.Current = None) -> float:
+        """Subtract two floats."""
+        return a - b
+
+    def multiply(self, a: float, b: float, current: Ice.Current = None) -> float:
+        """Multiply two floats."""
+        return a * b
+
+    def divide(self, a: float, b: float, current: Ice.Current = None) -> float:
+        """Divide two floats."""
+        if b == 0.0:
+            raise RemoteCalculator.ZeroDivisionError()
+        return a / b
