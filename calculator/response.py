d8fb72f1d5357fb69116204a12e7fd4dbbaeeaeb
jesuuscallejaas <jesus.callejas@alu.uclm.es>
2025-05-19 11:10:57 +0200

    feat: add response model

---
+++ calculator/response.py
@@ -0,0 +1,12 @@
+"""Response module for the calculator application."""
+
+from pydantic import BaseModel
+
+
+class CalculatorResponse(BaseModel):
+    """Response model for the calculator API."""
+
+    id: str
+    status: bool
+    result: float | None
+    error: str | None
